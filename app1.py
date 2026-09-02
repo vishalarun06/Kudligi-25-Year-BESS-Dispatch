49b76373365e648375509eab8dae18555dcad695
Vishal Arun <visharun06@gmail.com>
2026-09-02 12:41:30 +0530

    Delete app1.py

--- app1.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Wind-Solar+BESS 25 Year Simulations")
-st.title("Home Page")
-st.write("Use the sidebar to toggle between whether you plant sells to the exchange or not.")
