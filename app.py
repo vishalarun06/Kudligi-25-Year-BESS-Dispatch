f8f51edbbba8880cfc858060d7c6d99709b24856
Vishal Arun <visharun06@gmail.com>
2026-09-02 16:51:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -390,7 +390,9 @@ with tab_bess:
     with st.container(border=True):
         st.markdown("**BESS Sizing & Efficiency**")
         col8, col9, col10 = st.columns(3)
-        bess_capacity_kwh = col8.number_input("BESS Max SoC (kWh)", value=526_210.82, step=1000.0, format="%.2f")
+        scaling = 526210.82 / 5
+        bess_capacity_kwh = col8.number_input("BESS Hours of Storage", value=5, step=1, format="%.2f")
+        bess_capacity_kwh = bess_capacity_kwh * scaling
         pcs_cap = col9.number_input("PCS Power Cap (MW)", value=100.0, step=10.0)
         rte = col10.number_input("Round-Trip Efficiency (%)", min_value=0.0, max_value=100.0, value=84.78, step=0.5) / 100.0
 
